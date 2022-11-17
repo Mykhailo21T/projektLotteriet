5dd6b1b30db2b9a029c0adf01c619b2eadc2c6b0
MalteAndre <100594173+MalteAndre@users.noreply.github.com>
2022-11-17 14:51:14 +0100

    app setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, doc, deleteDoc, addDoc, getDoc, query, where } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,7 +21,11 @@ const appFirebase = initializeApp(firebaseConfig);
 const analytics = getAnalytics(appFirebase);
 
 const express = require('express')
-let app = express()
+const app = express()
+app.set('view engine', 'pug')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('assets'))
 
 
 let deltagerListe = []
