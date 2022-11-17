b645f653c35c8191ea20d9a49ea5ab9960fc65dc
Mykhailo21T <eaamysh@students.eaaa.dk>
2022-11-17 14:15:03 +0100

    1a

---
+++ test/app.test.js
@@ -0,0 +1,24 @@
+const assert = require('chai').assert
+const {addDeltager} = require('../app.js')
+
+let list = 
+beforeEach(function (){
+    list.length = 0
+})
+
+describe('When adding a Deltager', () =>{
+
+    it('Should add the deltager', () =>{
+
+        //assign
+        let konkretDeltager = "Knud Knudsen"
+        let konkretDeltager2 = "Per Hansen"
+        //act
+        addDeltager(konkretDeltager, list)
+        addDeltager(konkretDeltager2, list)
+
+        //assert
+        assert.equal(list.includes(konkretDeltager), "Knud knudsen")
+        assert.equal(list.includes(konkretDeltager2), "Per Hansen")
+    })
+})
