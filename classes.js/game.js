b3c655beb3e3950c1e900a6312e183e56e7200d2
spill62 <46723417+spill62@users.noreply.github.com>
2022-11-22 11:41:00 +0100

    findvinder

--- classes.js/game.js
+++ classes.js/game.js
@@ -1,8 +1,72 @@
 import { arrayUnion } from "firebase/firestore"
+let winnerArray = []
+let participantList = []
 class Game {
-    constructor(name) {
-        this.name = name
+     constructor(highestNum, lowestNum, amountOfWinningNums, date) {
+        this.highestNum = highestNum;
+        this.lowestNum = lowestNum;
+        this.amountOfWinningNums = amountOfWinningNums;
+        this.winnerArray = winnerArray;
+        this.date = date
+        this.participantList = participantList
     }
-
-
 }
+
+function setWinnerArrayForGame( winnerArray){
+    this.winnerArray = winnerArray;
+}
+
+
+//Antal vindertal er et en identifier til at finde ud af om der skal søges efter vindere med 3,2 eller 1 vindertal
+//Deltager listen er den samlet liste af alle deltagere
+//Vindertal er et array af 3 tal
+//Hver talrække hos hver deltager skal tjekkes. Rækkefølgen af tal i deltagerns talrække og vinderrækken er ikke sorteret
+//skal returnere et array med alle dem der har vundet (Deres faktisk deltager opjekt vi finder med findDeltager)
+
+
+function findVinder(){
+    let vindere = []
+    
+    let counter = 0
+    
+    
+    
+    for (let deltager of this.participantList) {
+        for (let talrækkeIndex = 0; talrækkeIndex<deltager.talrækker.length; talrækkeIndex++) {      
+            counter = 0
+    
+            for (let vindertalIndex = 0; vindertalIndex<this.amountOfWinningNums; vindertalIndex++){
+                console.log("counter: " + counter);
+                console.log("vindertal: " + antalVindertal);
+               
+           
+               
+               
+                for (let talIndex = 0; talIndex<5; talIndex++) {
+                    let talrække = deltager.talrækker[talrækkeIndex]
+                   
+                    if (talrække[talIndex] == winnerArray[vindertalIndex] ) {
+                        counter++ 
+                        break
+                    }
+                    }
+                }
+            }
+            if (counter == this.amountOfWinningNums) {
+                console.log("KOM HER INd");
+                console.log(deltager);
+                let xxx = deltager
+                vindere.push(xxx)
+                counter = 0
+                
+            }
+        }
+    
+    
+    
+       return vindere
+    }
+    
+
+    export {Game, findVinder, setWinnerArrayForGame}
+    
